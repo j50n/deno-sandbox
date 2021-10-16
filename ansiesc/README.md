6d0520dc056934ec9e1c9f17586841b8128ce555
j50n <j50n@lilypepper.com>
2021-10-16 08:50:53 -0600

    stubs for new projects

---
+++ ansiesc/README.md
@@ -0,0 +1,4 @@
+# Something
+
+## Ref
+  - [ANSI escape code (Wikipedia)](https://en.wikipedia.org/wiki/ANSI_escape_code)
--- bin/punchin.ts
+++ bin/punchin.ts
@@ -7,6 +7,8 @@ import { format, parseInterval } from "../lib/punchin/interval.ts";
 import { setup } from "../lib/punchin/setup.ts";
 import { sleep } from "../lib/util.ts";
 
+const MINUTE = 60 * 1000;
+
 setup();
 
 let totalTimeSoFar = 0;
@@ -37,25 +39,33 @@ async function refreshDisplay(): Promise<void> {
   }
 }
 
+async function tryUpdateTime(): Promise<void> {
+  const now = new Date().getTime();
+  if (!onBreak) {
+    totalTimeSoFar += now - lastTimeIChecked;
+  }
+  lastTimeIChecked = now;
+
+  const interval = format(totalTimeSoFar / 1000);
+  if (interval !== timeWorkedDisplay) {
+    timeWorkedDisplay = interval;
+    await refreshDisplay();
+  }
+}
+
 (async () => {
   while (true) {
-    const now = new Date().getTime();
-    if (!onBreak) {
-      totalTimeSoFar += now - lastTimeIChecked;
-    }
-    lastTimeIChecked = now;
-
-    const interval = format(totalTimeSoFar / 1000);
-    if (interval !== timeWorkedDisplay) {
-      timeWorkedDisplay = interval;
-      await refreshDisplay();
-    }
+    tryUpdateTime();
     await sleep(1000);
   }
 })().catch((e) => {
   console.error(e);
   Deno.exit(2);
 });
+
+function minuteFloor(time: number): number {
+  return Math.floor(time / MINUTE) * MINUTE;
+}
 
 (async () => {
   for await (const keypress of readKeypress()) {
@@ -66,6 +76,20 @@ async function refreshDisplay(): Promise<void> {
     if (keypress.key === "space") {
       onBreak = !onBreak;
       await refreshDisplay();
+    }
+
+    if (!onBreak) {
+      if (keypress.key === "up") {
+        totalTimeSoFar = minuteFloor(totalTimeSoFar) + MINUTE;
+        lastTimeIChecked = new Date().getTime();
+        await tryUpdateTime();
+      }
+
+      if (keypress.key === "down") {
+        totalTimeSoFar = minuteFloor(totalTimeSoFar) - MINUTE;
+        lastTimeIChecked = new Date().getTime();
+        await tryUpdateTime();
+      }
     }
   }
 })().catch((e) => {
